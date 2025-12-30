6ac472f6a7dca46a9550e711d27b1312712d4300
M0diis <modestas.kazlauskas@ktu.edu>
2025-12-30 16:11:11 +0200

    Why is there a semicolon

--- src/main/java/me/m0dii/utils/TickHandler.java
+++ src/main/java/me/m0dii/utils/TickHandler.java
@@ -6,7 +6,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 public class TickHandler {
     public static void register() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            ;
             CameraEntity.movementTick();
         });
     }
